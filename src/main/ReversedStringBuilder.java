810fc1f07052c907f92dec4b53fa0b106fac3d89
Tao Wu <tl2wu@uwaterloo.ca>
2016-11-10 21:10:45 -0500

    added the insert(int offset, ...) methods

--- src/main/ReversedStringBuilder.java
+++ src/main/ReversedStringBuilder.java
@@ -66,7 +66,7 @@ public class ReversedStringBuilder
 	}
 
 	public void setCharAt(int index, char ch) {
-		if((index < 0) || (index >= count)) {
+		if((index < 0) || (index >= length())) {
 			throw new StringIndexOutOfBoundsException(index);
 		}
 		value[value.length - count + index] = ch;
@@ -145,10 +145,94 @@ public class ReversedStringBuilder
 		}
 	}
 
+	public ReversedStringBuilder insert(int offset, char c) {
+		if ((offset < 0) || (offset > length())) {
+			throw new StringIndexOutOfBoundsException(offset);
+		}
+		
+		ensureCountIncrease(1);
+		arraycopyForward(value, value.length-count, value, value.length-count-1, offset+1);
+		value[offset] = c;
+		count++;
+		return this;
+	}
+	
+	public ReversedStringBuilder insert(int offset, char[] s) {
+		if ((offset < 0) || (offset > length())) {
+			throw new StringIndexOutOfBoundsException(offset);
+		}
+		
+		ensureCountIncrease(s.length);
+		arraycopyForward(value, value.length-count, value, value.length-count-s.length, offset+1);
+		count += s.length;
+		arraycopyForward(s, 0, value, value.length+offset, s.length);
+		return this;
+	}
+	
+	public ReversedStringBuilder insert(int offset, char[] s, int start, int end) {
+		if ((offset < 0) || (offset > length())) {
+			throw new StringIndexOutOfBoundsException(offset);
+		}
+		
+		ensureCountIncrease(s.length);
+		arraycopyForward(value, value.length-count, value, value.length-count-s.length, offset+1);
+		count += s.length;
+		arraycopyForward(s, 0, value, value.length+offset, s.length);
+		return this;
+	}
+	
+	public ReversedStringBuilder insert(int dstOffset, CharSequence s) {
+		if ((dstOffset < 0) || (dstOffset > length())) {
+			throw new StringIndexOutOfBoundsException(dstOffset);
+		}
+		if(s == null) {
+			s = "null";
+		}
+		
+		ensureCountIncrease(s.length());
+		arraycopyForward(value, value.length-count, value, value.length-count-s.length(), dstOffset+1);
+		for(int i=0; i<s.length(); i++) {
+			value[dstOffset++] = s.charAt(i);
+		}
+		count += s.length();
+		return this;
+	}
+	
+	public ReversedStringBuilder insert(int dstOffset, CharSequence s, int start, int end) {
+		if ((dstOffset < 0) || (dstOffset > length())) {
+			throw new StringIndexOutOfBoundsException(dstOffset);
+		}
+		if(s == null) {
+			s = "null";
+		}
+		boundsChecking(start, end, s.length());
+		
+		int insertedlength = end - start;
+		ensureCountIncrease(insertedlength);
+		arraycopyForward(value, value.length-count, value, value.length-count-insertedlength, insertedlength);
+		for(int i=start ; i<end; i++) {
+			value[dstOffset++] = s.charAt(i);
+		}
+		count += insertedlength;
+		return this;
+	}
+	
 	public ReversedStringBuilder clone() {
 		ReversedStringBuilder clonedSb = new ReversedStringBuilder(this.length());
 		clonedSb.append(this.value, value.length-count, value.length);
 		return clonedSb;
+	}
+	
+	private void arraycopyForward(char[] src, int srcPos, char[] dest, int destPos, int length) {
+		for(int i=0; i<length; i++){
+			 dest[destPos+i] = src[srcPos+i];
+		}
+	}
+	
+	private void arraycopyBackword(char[] src, int srcPos, char[] dest, int destPos, int length) {
+		for(int i=length-1; i>=0; i--){
+			dest[destPos+i] = src[srcPos+i];
+		}
 	}
 	
 	private void boundsChecking(int start, int end, int len) {
